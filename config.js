7e2b54e2c1a626b4d9969b5c2186a84947c93f8d
Leonardo Uieda <leouieda@gmail.com>
2021-02-21 14:35:24 +0000

    Remove the leading /caroldoscopio in the layouts

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
-  
+
     // Copy these folder to _site without converting the files in them
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("assets");
 
-  };
+};
